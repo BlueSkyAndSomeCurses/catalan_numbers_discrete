819f3abcb78bee42e334a797796bd880295492f1
Valihurskyi Anton <sdafxzcv@noreply.com>
2023-11-27 11:41:33 +0200

    Catalan numbers recursive dynamic programming implementation

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 """
 Catalan
 """
+import functools
+import time
+
+
+@functools.lru_cache()
+def catalan_recursive(n: int):
+    match n:
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case 2:
+            return 2
+        case 3:
+            return 5
+
+    catalan = [0 for i in range(n + 1)]
+
+    catalan[0] = 1
+    catalan[1] = 1
+    for i in range(2, n + 1):
+        for k in range(i):
+            catalan[i] += catalan_recursive(i - k - 1) * catalan_recursive(k)
+
+    return catalan[n]
+
+
+def recursive_definition(n):
+    for i in range(n + 1):
+        print(i, ":", catalan_recursive(i))
 
 
 if __name__ == "__main__":
-    pass
+    recursive_definition(1000)
