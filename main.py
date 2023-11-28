28f4703316e3c3b67a093f80468ce76da4f96248
LidaSemsichko <149298455+LidaSemsichko@users.noreply.github.com>
2023-11-28 12:27:51 +0200

    Added function ways_to_dot

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 Catalan
 """
+def factorial_recursive(number):
+    '''
+    Recursive function, returns a factorial of number
+    '''
+    factorial = 1
+    if number != 1:
+        factorial *= (number * (factorial_recursive(number - 1)))
+    else:
+        return 1
+    return factorial
 
+
+def ways_to_dot(n_elem):
+    '''
+    Returns number of ways from (0,0) to (n,n)
+    '''
+    result = (factorial_recursive(2* n_elem) // \
+(factorial_recursive(n_elem) * factorial_recursive(2* n_elem - n_elem))) // (n_elem + 1)
+    return result
 
 if __name__ == "__main__":
     pass
