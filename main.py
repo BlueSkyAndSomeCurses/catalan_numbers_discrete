5c9e7b36042316323d032943be2d29f4de998189
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2023-11-28 09:27:52 +0200

    parentheses

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 Catalan
 """
 
+def main(n: int) -> list[str]:
+    res = []
+    def dfs(left, right, s):
+        if len(s) == n * 2:
+            res.append(s)
+            return
+        if left < n:
+            dfs(left + 1, right, s + '(')
+        if right < left:
+            dfs(left, right + 1, s + ')')
+    dfs(0, 0, '')
+    return res
+
+p=main(10)
+for i in p:
+    print(i)
 
 if __name__ == "__main__":
     pass
